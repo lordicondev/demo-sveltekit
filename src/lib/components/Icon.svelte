aba3aab25350f5e591a4c0534b3a7dfb253fb2cf
Marcin Baszczewski <marcin@baszczewski.pl>
2024-08-09 11:08:59 +0200

    Init.

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from 'svelte';
+  import lottie from "lottie-web";
+  
+  onMount(async () => {
+    const { defineElement } = await import('@lordicon/element');
+
+    // define "lord-icon" custom element with default properties
+    defineElement(lottie.loadAnimation);
+  });
+    
+  export let name = "puzzle";
+
+  const src = `/${name}.json`;
+</script>
+
+<style>
+  lord-icon {
+    width: 128px;
+    height: 128px;
+  }
+</style>
+
+<lord-icon
+    {src}
+    trigger="hover">
+</lord-icon>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Icon from '$lib/components/Icon.svelte';
+</script>
+
+<h1>SvelteKit with Lordicon!</h1>
+
+<Icon name="puzzle" />
+<Icon name="lock" />
